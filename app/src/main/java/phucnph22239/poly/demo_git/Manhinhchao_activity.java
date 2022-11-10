8ca7c5c3d94505d64e891c2c932ae66c29fcb931
Nguyen chi sang <sangncph25176@fpt.edu.vn>
2022-11-10 13:26:58 +0700

    sangncph25176 man hinh chao

---
+++ app/src/main/java/phucnph22239/poly/demo_git/Manhinhchao_activity.java
@@ -0,0 +1,14 @@
+package phucnph22239.poly.demo_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Manhinhchao_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhchao);
+    }
+}
